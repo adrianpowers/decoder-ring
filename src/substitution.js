feab26d111b84808e98e77b3f6a47a1a3968448d
Adrian Powers <adrianpowers530@gmail.com>
2022-10-14 16:26:15 -0400

    substitution encode working

--- src/substitution.js
+++ src/substitution.js
@@ -1,18 +1,47 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
-  function substitution(input, alphabet, encode = true) {
-    // your solution code here
+  function substitution(input, alphabet = "", encode = true) {
+    if (alphabet.length !== 26) return false;
+
+    /* this block uses key/value pairs to check if a character 
+    in the given alphabet cipher has been repeated;
+    in the loop, it gets the key for that letter in the object:
+    if it is true, that means it already exists and thus, the alphabet isn't unique;
+    if it is false, the key is set to true, and it now exists */
+
+    let alphaUniqueCheck = {};
+    for (let i = 0; i < alphabet.length; i++) {
+      let letterKey = alphabet[i];
+      if (alphaUniqueCheck[letterKey]) return false;
+      alphaUniqueCheck[letterKey] = true;
+    }
+
+    let splitAlpha = alphabet.split("");
+    let splitMsg = input.toLowerCase().split("");
+
+    if (encode) {
+      splitMsg = splitMsg.map((letter) => {
+        if (letter.charCodeAt(0) < 97 || letter.charCodeAt(0) > 122)
+          return letter;
+        return splitAlpha[letter.charCodeAt(0) - 97];
+      });
+
+      return splitMsg.join("");
+    }
+
+    if (!encode) {
+
+    }
   }
 
   return {
     substitution,
   };
 })();
+
+console.log(
+  substitutionModule.substitution("message", "$wae&zrdxtfcygvuhbijnokmpl", false)
+);
 
 module.exports = { substitution: substitutionModule.substitution };
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,3 +1,18 @@
 const { expect } = require("chai");
 const { substitution } = require("../src/substitution");
 
+describe("Adrian's substitution tests", () => {
+    it(("should have an alphabet with exactly 26 characters"), () => {
+        let actual = substitution("abc", "short")
+        expect(actual).to.be.false;
+    });
+    it(("every character in alphabet should be unique; otherwise, return false"), () => {
+        let actual = substitution("abc", "abcdefghijklmnopqrstuvwxyx");
+        expect(actual).to.be.false;
+    });
+    it(("should encode a message regardless of spaces, capitals, and special characters"), () => {
+        let expected = "jrufscpw xoy";
+        let actual = substitution("ThinKFul aBc", "xoyqmcgrukswaflnthdjpzibev");
+        expect(actual).to.be.equal(expected);
+    });
+})
